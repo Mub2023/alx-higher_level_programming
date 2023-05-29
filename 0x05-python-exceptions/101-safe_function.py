c2817af6c2011b4a574248977ba6e8bf2183268d
Mub2023 <mubatta@gmail.com>
2023-05-29 11:19:28 -0700

    M_U B_2 0_2 3_Task 0_8 FIX T_Y

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,8 +2,8 @@
 def safe_function(fct, *args):
     import sys
     try:
-            result = fct(*args)
-            return result
+        result = fct(*args)
+        return result
     except Exception as d:
-            print("Exception: {}".format(d), file=sys.stderr)
-            return None
+        print("Exception: {}".format(d), file=sys.stderr)
+        return None
