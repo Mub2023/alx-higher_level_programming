2b0f4c8c640d292a8a32a7ca60131e463bb092f3
Mub2023 <mubatta@gmail.com>
2023-05-30 14:19:49 -0700

    M_U B_2 0_2 3_Task 0_5 FIX T_Y

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines class Square"""
+
+
+class Square:
+    """An new Square."""
+
+    def __init__(self, size=0):
+        """Initiaze the new class and its args.
+
+        Args:
+        size: The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the current size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of square ."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints the square with #."""
+        for x in range(0, self.__size):
+            for y in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
