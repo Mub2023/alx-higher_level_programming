5981d9e4525a738a77f6014bd4eaddc04c6ad9a0
Mub2023 <mubatta@gmail.com>
2023-05-30 13:39:03 -0700

    M_U B_2 0_2 3_Task 0_4 FIX T_Y

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """an new Square."""
+    """An new Square."""
 
     def __init__(self, size=0):
         """Initiaze the new class and its args.
@@ -19,7 +19,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """sets the current size of the square."""
+        """Sets the current size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
