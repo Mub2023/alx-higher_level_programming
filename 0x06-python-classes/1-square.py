c7d024a1ca56d47771a90b2a27354ece989a0a53
Mub2023 <mubatta@gmail.com>
2023-05-30 12:36:29 -0700

    M_U B_2 0_2 3_Task 0_1 FIX T_Y

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class as Square."""
+
+
+class Square:
+    """inside the square."""
+
+    def __init__(self, size):
+        """initialize the square.
+
+        Args:
+        size: The size of new square.
+        """
+        self.__size = size
