8277058251919435124f50245c8e4977b9c5039f
Mub2023 <mubatta@gmail.com>
2023-05-30 12:51:25 -0700

    M_U B_2 0_2 3_Task 0_2 FIX T_Y

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,10 +8,10 @@ class Square:
     def __init__(self, size=0):
         """Initiaize a new Square.
         Args:
-        size: Private instance attribute the size of Square
+        size: Private instance attribute the size of Square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size == size
+        self.__size = size
