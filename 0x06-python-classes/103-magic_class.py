bbecab1bc7ee9ab7c0060f9e882775e2ead868f3
Mub2023 <mubatta@gmail.com>
2023-05-30 17:58:24 -0700

    M_U B_2 0_2 3_Task 1_0 FIX T_Y

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Define a MagicClass that does exactly the same as python bytecode"""
+
+
+class MagicClass:
+    """initilazile the magic magic magic class"""
+
+    def __init__(self, radius=0):
+        """initilazile the magic magic class
+
+        Arg:
+        radius: The radius of the new magic class
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self._radius = radius
+
+    def area(self):
+        """Return the area of the magic magic class"""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """Return The circumference of the magic magic class"""
+        return (2 * math.pi * self.__radius)
