ce8e44fe5a887843a0708d2dbf3de5352478e9c8
mub2023 <mubatta@gmail.com>
2023-06-12 23:14:54 +0200

    M_U B_2 0_2 3_Task 0_6 FIX T_Y

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """new BaseGeometry class !!!"""
+    def area(self):
+        """raises an Exception Error"""
+        raise Exception("area() is not implemented")
