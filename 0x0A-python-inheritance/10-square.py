b1d574a4156c13572572f8b8a88afc5f92f63fba
mub2023 <mubatta@gmail.com>
2023-06-13 00:57:11 +0200

    M_U B_2 0_2 3_Task 1_0 FIX T_Y

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a class Square that inherits
+Rectangle (9-rectangle.py) i cant get my inhertance.
+"""
+Rectangle = __import__ ('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherit
+    Rectangle class and change to 
+    Square class ^_^"""
+    def __init__(self, size):
+        """change to
+        Square
+
+        Args:
+        size (int): the size of the Square.
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calaulates thearea of the Square"""
+        return self.__size * self.__size
