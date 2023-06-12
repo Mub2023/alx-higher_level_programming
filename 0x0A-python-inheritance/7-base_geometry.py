a46a59e9529cf650918d6b6205136cc310812e54
mub2023 <mubatta@gmail.com>
2023-06-12 23:52:52 +0200

    M_U B_2 0_2 3_Task 0_7 FIX T_Y

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,6 +7,7 @@ class BaseGeometry:
     def area(self):
         """raises an Exception Error"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Public instance method:
         Args:
