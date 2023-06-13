ff3518416e3ead8f061408c41b69b18330d36b99
mub2023 <mubatta@gmail.com>
2023-06-13 19:37:34 +0200

    M_U B_2 0_2 3_Task 0_0 FIX T_Y

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ prints it to stdout:"""
 def read_file(filename=""):
     """open a file wth UTF8 and
     prints it to stdout"""
-    with open("my_file_0.txt", "r", encoding="utf8") as x:
+    with open(filename, encoding="utf8") as x:
         string = x.read()
         print(string)
