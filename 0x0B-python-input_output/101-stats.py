e1a64f639bac82b3d6730a048035e401bf7b2902
mub2023 <mubatta@gmail.com>
2023-06-14 01:23:18 +0200

    M_U B_2 0_2 3_Task 1_4 FIX T_Y

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,40 +4,37 @@ line by line and computes metrics"""
 import sys
 
 
-def print_stat(size, status_code):
+def print_stat(size, known_codes):
     """print the stats of size and status"""
     print("File size: {}".format(size))
-    for x in sorted(status_code):
-        print("{}: {}".format(key, status_code[x]))
-if __name__ == "__main__":
+    for x, v in sorted(known_codes.items()):
+        if v != 0:
+            print("{}: {}".format(x, v))
 
-    size = 0
-    status_code = {}
-    known_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
-    cn = 0
 
-    try:
-        for wi in sys.stdin:
-            if cn == 10:
-                print_stat(size, status_code)
-                cn = 1
-            else:
-                cn += 1
-            wi = wi.split()
+size = 0
+known_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+               '403': 0, '404': 0, '405': 0, '500': 0}
+it = 0
+
+try:
+    for wi in sys.stdin:
+        it += 1
+        wi = wi.split()
+        if len(wi) >= 2:
+            temp = it
+            if wi[-2] in known_codes:
+                known_codes[wi[-2]] += 1
+                it += 1
             try:
                 size += int(wi[-1])
-            except (IndexError, ValueError):
-                pass
-            try:
-                if wi[-2] in known_codes:
-                    if status_code.get(wi[-2], 0) == 0:
-                        status_code[wi[-2]] = 1
-                    else:
-                        status_code[wi[-2]] += 1
+                if temp == it:
+                    it += 1
             except IndexError:
                 pass
+        if it % 10 == 0:
+            print_stat(size, known_codes)
 
-        print_stat(size, status_code)
-    except KeyboardInterrupt:
-        print_stat(size, status_code)
-        raise
+    print_stat(size, known_codes)
+except KeyboardInterrupt:
+    print_stat(size, known_codes)
