08c8f317275a2ecad74fffb10370ce45f78b5fbc
mub2023 <mubatta@gmail.com>
2023-07-12 00:14:09 +0300

    M_U B_2 0_2 3_Task A_L_L FIX T_Y

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,40 +3,47 @@
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
-from collections import Counter
-
-
-def print_stats(size, Scode):
+def print_stats(size, s_codes):
     """Print accumulated metrics."""
     print("File size: {}".format(size))
-    for k in sorted(Scode):
-        print("{}: {}".format(k, Scode[k]))
+    for key in sorted(s_codes):
+        print("{}: {}".format(key, s_codes[key]))
 
 
 if __name__ == "__main__":
     import sys
 
     size = 0
-    Scode = Counter()
-    ct = 0
+    s_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
-    with sys.stdin as f:
-        try:
-            for line in f:
-                if ct == 10:
-                    print_stats(size, Scode)
-                    ct = 0
-                ct += 1
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-                line = line.split()
+            line = line.split()
 
-                try:
-                    size += int(line[-1])
-                    Scode[line[-2]] += 1
-                except (IndexError, ValueError):
-                    pass
-            print_stats(size, Scode)
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
-        except KeyboardInterrupt:
-            print_stats(size, Scode)
-            raise
+            try:
+                if line[-2] in valid_codes:
+                    if s_codes.get(line[-2], -1) == -1:
+                        s_codes[line[-2]] = 1
+                    else:
+                        s_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, s_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, s_codes)
+        raise
