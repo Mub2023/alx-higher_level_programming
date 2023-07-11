d31ccb610d38dfb2fc600a3a1750dae6d364f008
mub2023 <mubatta@gmail.com>
2023-07-12 00:53:43 +0300

    M_U B_2 0_2 3_Task A_L_L FIX T_Y

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,47 +3,41 @@
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
+from collections import Counter
 
 
 def print_stats(size, Scode):
     """Print accumulated metrics."""
-    print("File size: {}".format(size))
-    for ka in sorted(Scode):
-        print("{}: {}".format(ka, Scode[ka]))
+    print(f"File size: {size}")
+    for k in sorted(Scode):
+        print(f"{k}: {Scode[k]}")
 
 
 if __name__ == "__main__":
     import sys
 
     size = 0
-    Scode = {}
-    known_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    Scode = Counter()
     ct = 0
 
-    try:
-        for line in sys.stdin:
-            if ct == 10:
-                print_stats(size, Scode)
-                ct = 0
-            else:
+    with sys.stdin as f:
+        try:
+            for line in f:
+                if ct == 10:
+                    print_stats(size, Scode)
+                    ct = 1
                 ct += 1
 
-            line = line.split()
+                line = line.split()
 
-            try:
-                size += int(line[-1])
-            except(IndexError, ValueError):
-                pass
-            try:
-                if line[-2] in known_codes:
-                    if Scode.get(line[-2], -1) == -1:
-                        Scode[line[-2]] = 1
-                    else:
-                        Scode[line[-2]] += 1
-            except IndexError:
-                pass
-        print_stats(size, Scode)
+                try:
+                    size += int(line[-1])
+                    Scode[line[-2]] += 1
+                except (IndexError, ValueError):
+                    pass
+            else:
+                print_stats(size, Scode)
 
-    except KeyboardInterrupt:
-        print_stats(size, Scode)
-        raise
+        except KeyboardInterrupt:
+            print_stats(size, Scode)
+            raise
