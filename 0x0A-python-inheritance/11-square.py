d10e5e6ee2f174e94e26dd4774b12d1b73dbfd5e
mub2023 <mubatta@gmail.com>
2023-07-11 05:18:43 +0300

    M_U B_2 0_2 3_Task A_L_L FIX T_Y

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,29 +2,25 @@
 """
 a class Square that inherits
 Rectangle (9-rectangle.py) i cant get my inhertance.
-change it to square
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Square class that inherit
+    """Square class that inherit
     Rectangle class and change to
-    Square class"""
+    Square class ^_^"""
     def __init__(self, size):
-        """change to Square
+        """change to
+        Square
+
         Args:
         size (int): the size of the Square.
         """
-        self.__size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
-        """return, the square description:"""
-        return("[Square] {:d}/{:d}".format(self.__size, self.__size))
-
-    def area(self):
-        """Return the area of the square"""
-        return super().area()
+        """should return, the square description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
